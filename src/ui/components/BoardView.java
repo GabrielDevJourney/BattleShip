046061b15fe50f6ca3fdb3a76c5cf48949a39cc8
Gabriel <gabspereira178@gmail.com>
2025-01-03 15:51:12 +0000

    General: board view creation and implementation
    Specific :
    - logic to create gird based on cells that will be clickable so player can place his ships into his board,
    - initilizing board with proper water state to all grid cells
    - enable shadow coloring to cells to where ship can be placed
    - clear those same shadows and update cells state

--- src/ui/components/BoardView.java
+++ src/ui/components/BoardView.java
@@ -1,4 +1,71 @@
 package src.ui.components;
 
-public class BoardView {
+import javafx.scene.control.Label;
+import javafx.scene.layout.GridPane;
+import src.enums.BoardState;
+import src.utils.Coordinate;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.Consumer;
+
+public class BoardView extends GridPane {
+
+	private final int boardSize;
+	private Label[][] cells;  // For displaying cell states (water/ship/shadow)
+	private Consumer<Coordinate> onCellClicked;
+
+	public BoardView(int boardSize) {
+		this.boardSize = boardSize;
+		initializeGrid();
+	}
+
+	public int getBoardSize() {
+		return boardSize;
+	}
+
+	private void initializeGrid() {
+		//setup in placement screen the player board view to utilize label to be able to display proper emojis
+		cells = new Label[boardSize][boardSize];
+		for (int i = 0; i < boardSize; i++) {
+			for (int j = 0; j < boardSize; j++) {
+				Label cell = new Label(BoardState.WATER.getBoardState());
+				cell.getStyleClass().add("grid-cell");
+				cells[i][j] = cell;
+				add(cell, j, i);
+
+				final int row = i;
+				final int col = j;
+				cell.setOnMouseClicked(event -> {
+					if (onCellClicked != null) {
+						onCellClicked.accept(new Coordinate(row, col));
+					}
+				});
+			}
+		}
+	}
+
+	public void displayAllShadowPlacements(List<List<Coordinate>> allPlacements) {
+		clearShadows();
+		allPlacements.forEach(placement -> {
+			placement.forEach(coordinate -> {
+				cells[coordinate.getRow()][coordinate.getCol()].getStyleClass().add("shadow-placement");
+
+			});
+
+		});
+	}
+
+	public void clearShadows() {
+		Arrays.stream(cells).flatMap(Arrays::stream)
+				.forEach(cell -> cell.getStyleClass().remove("shadow-placement"));
+	}
+
+	public void updateCellState(Coordinate coordinate, BoardState state) {
+		cells[coordinate.getRow()][coordinate.getCol()].setText(state.getBoardState());
+	}
+
+	public void setOnCellClicked(Consumer<Coordinate> onCellClicked) {
+		this.onCellClicked = onCellClicked;
+	}
 }
